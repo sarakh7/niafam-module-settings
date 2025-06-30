b5a7c08c85dbdaa676866d799f5aded9a0898d94
Sara <41621655+sarakh7@users.noreply.github.com>
2025-06-30 17:54:28 +0330

    feat:add es font icons and some scss files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { initGallery } from "./features/gallery";
 import { initAudioPlayer, initVideoPlayer } from "./features/mediaPlayer";
+//import "esfonticon/style.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
   initGallery();
